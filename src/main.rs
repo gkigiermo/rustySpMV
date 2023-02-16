ce8fe4653c09aac7567016deba5a704b3ba7626d
Guillermo Oyarzun <guillermo.oyarzun@bsc.es>
2023-02-17 00:18:58 +0100

    handling input arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::Parser;
+
+#[derive(Parser,Default,Debug)]
+#[clap(author="Guillermo Oyarzun", version, about)]
+///A sparse matrix-vector multiplication in CSR format
+struct Arguments {
+    #[clap(short, long)]
+    #[clap(forbid_empty_values = true)]
+    /// Matrix file in CSR format
+    matrix_file: String,
+}
 fn main() {
-    println!("Hello, world!");
+    let args = Arguments::parse();
+    println!("matrix_name {}", args.matrix_file);
+   
+    //Reading a file
+
+    //Create a datastructure/class 
+
+    //create the spmv kernel
+    
+    //measure times 
+
 }
