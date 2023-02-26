00139f2a0ccc996d0c88848b93ed6d5ee7effd4f
Guillermo Oyarzun <guillermo.oyarzun@bsc.es>
2023-02-26 21:58:00 +0100

    Read the matrix and fill matrix structure

--- src/main.rs
+++ src/main.rs
@@ -13,57 +13,86 @@ struct Arguments {
     matrix_file: String,
 }
 
-fn read_file_line_by_line(filepath: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let file = File::open(filepath)?;
-    let reader = BufReader::new(file);
+struct SparseMatrix{
+    matrix_params : Vec<i32>,
+    vals : Vec<f64>,
+    cols : Vec<i32>,
+    row_ptr: Vec<i32>,
+}
 
-    //Matrix params and values::: missing convert this into an optimal data structure
-    let mut matrix_params: Vec<i32> = Vec::new();
-    let mut matrix_vals: Vec<f64> = Vec::new();
-    let mut matrix_cols: Vec<i32> = Vec::new();
-    let mut matrix_row_ptrs: Vec<i32> = Vec::new();
-    let mut index : i32 = 0;
+impl SparseMatrix{
+    //Creating an empty sparse matrix
+    pub fn new()-> Self{
+        let matrix_params:  Vec<i32> = Vec::new();
+        let vals:  Vec<f64> = Vec::new();
+        let cols:  Vec<i32> = Vec::new();
+        let row_ptr:  Vec<i32> = Vec::new();
 
-    for line in reader.lines() {
-        for word in line?.split(' '){
-            if index == 0 {
-                matrix_params.push(word.parse::<i32>().unwrap());
-            } else if index == 1 {
-                matrix_vals.push(word.parse::<f64>().unwrap());
-            } else if index == 2 {
-                matrix_cols.push(word.parse::<i32>().unwrap());
-            } else if index == 3 {
-                matrix_row_ptrs.push(word.parse::<i32>().unwrap());
-            }
+        Self{
+            matrix_params,
+            vals,
+            cols,
+            row_ptr,
         }
-        index += 1;
+    }
+    // Reading the matrix in the current format
+    pub fn read_matrix_from_file(&mut self, filepath: &str)-> Result<(), Box<dyn std::error::Error>> {
+        let file = File::open(filepath)?;
+        let reader = BufReader::new(file);
+        
+        let mut index : i32 = 0;
+        for line in reader.lines() {
+            for word in line?.split(' '){
+                if index == 0 {
+                    self.matrix_params.push(word.parse::<i32>().unwrap());
+                } else if index == 1 {
+                    self.vals.push(word.parse::<f64>().unwrap());
+                } else if index == 2 {
+                    self.cols.push(word.parse::<i32>().unwrap());
+                } else if index == 3 {
+                    self.row_ptr.push(word.parse::<i32>().unwrap());
+                }
+            }
+            index += 1;
+        }
+
+        Ok(())
     }
 
-    // Just a small check
-    for i in 0..5 {
-        println!("param: {}", matrix_params[i] );
-        println!("val: {}", matrix_vals[i] );
-        println!("col: {}", matrix_cols[i] );
-        println!("row_ptr:{}", matrix_row_ptrs[i] );
-    }
+    pub fn print_first_n_lines(&mut self, n: usize) -> Result<(), Box<dyn std::error::Error>>{
+        for i in 0..self.matrix_params.len(){
+            println!(" {}", self.matrix_params[i]);
+        }
+        for i in 0..n {
+            println!("val[{}]: {}", i,  self.vals[i] );
+            println!("col[{}]: {}", i, self.cols[i] );
+            println!("row_ptr[{}]:{}", i, self.row_ptr[i] );
+        }
 
-    Ok(())
+        Ok(())
+    }
 }
 
 fn main() {
+    //Read the arguments given in the console
     let args = Arguments::parse();
-    let result = read_file_line_by_line(&args.matrix_file);
+
+    //Create an empty matrix
+
+    let mut sp_matrix = SparseMatrix::new();
     
-    match result {
-        Ok(()) => println!("Matrix read correctly"),
-        Err(e) => println!("Error: {}", e ),
+    //Fill the structure with the matrix information from the file
+    match &sp_matrix.read_matrix_from_file(&args.matrix_file){
+        Ok(()) => println!("Matrix from struct read correctly"),
+        Err(e) => println!("Error: {}", e ),    
     }
-    //println!("matrix_name {}", args.matrix_file);
-   
-    //Reading a file
-
-    //Create a datastructure/class 
-
+    
+    //Just a check to print the first n elements in the vectors
+    match sp_matrix.print_first_n_lines(1){
+        Ok(()) => {},
+        Err(e) => println!("Error: {}", e ),  
+    }
+    
     //create the spmv kernel
     
     //measure times 
