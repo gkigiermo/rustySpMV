d6cd591e643fac948cadcbecc484b57ef0fadf94
Guillermo Oyarzun <guillermo.oyarzun@bsc.es>
2023-02-27 00:23:47 +0100

    Adding dense vectors

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,13 @@ use std::io::BufRead;
 
 #[derive(Parser,Default,Debug)]
 #[clap(author="Guillermo Oyarzun", version, about)]
-/// A sparse matrix-vector multiplication in CSR format
+/// A sparse matrix-vector multiplication in CSR format.
+/// Calculates: y = A * x
+/// , where:
+/// A is a sparse matrix in CSR format,
+/// x is a dense vector with as many components as columns in the sparse matrix,
+/// y is a dense vector with as many components as rows in the sparse matrix.
+
 struct Arguments {
     #[clap(short, long)]
     #[clap(forbid_empty_values = true)]
@@ -14,19 +20,19 @@ struct Arguments {
 }
 
 struct SparseMatrix{
-    matrix_params : Vec<i32>,
+    matrix_params : Vec<usize>,
     vals : Vec<f64>,
-    cols : Vec<i32>,
-    row_ptr: Vec<i32>,
+    cols : Vec<usize>,
+    row_ptr: Vec<usize>,
 }
 
 impl SparseMatrix{
     //Creating an empty sparse matrix
     pub fn new()-> Self{
-        let matrix_params:  Vec<i32> = Vec::new();
+        let matrix_params:  Vec<usize> = Vec::new();
         let vals:  Vec<f64> = Vec::new();
-        let cols:  Vec<i32> = Vec::new();
-        let row_ptr:  Vec<i32> = Vec::new();
+        let cols:  Vec<usize> = Vec::new();
+        let row_ptr:  Vec<usize> = Vec::new();
 
         Self{
             matrix_params,
@@ -44,19 +50,27 @@ impl SparseMatrix{
         for line in reader.lines() {
             for word in line?.split(' '){
                 if index == 0 {
-                    self.matrix_params.push(word.parse::<i32>().unwrap());
+                    self.matrix_params.push(word.parse::<usize>().unwrap().try_into().unwrap());
                 } else if index == 1 {
                     self.vals.push(word.parse::<f64>().unwrap());
                 } else if index == 2 {
-                    self.cols.push(word.parse::<i32>().unwrap());
+                    self.cols.push(word.parse::<usize>().unwrap().try_into().unwrap());
                 } else if index == 3 {
-                    self.row_ptr.push(word.parse::<i32>().unwrap());
+                    self.row_ptr.push(word.parse::<usize>().unwrap().try_into().unwrap());
                 }
             }
             index += 1;
         }
 
         Ok(())
+    }
+
+    pub fn get_rows_number(&mut self) -> usize {
+        self.matrix_params[2]
+    }
+
+    pub fn get_columns_number(&mut self) -> usize {
+        self.matrix_params[1]
     }
 
     pub fn print_first_n_lines(&mut self, n: usize) -> Result<(), Box<dyn std::error::Error>>{
@@ -73,12 +87,17 @@ impl SparseMatrix{
     }
 }
 
+pub fn fill_vector(_vector : &mut Vec<f64>) {
+    for index in 0.._vector.len() {
+        _vector[index] = 0.1;
+    }
+}
+
 fn main() {
     //Read the arguments given in the console
     let args = Arguments::parse();
 
     //Create an empty matrix
-
     let mut sp_matrix = SparseMatrix::new();
     
     //Fill the structure with the matrix information from the file
@@ -93,8 +112,19 @@ fn main() {
         Err(e) => println!("Error: {}", e ),  
     }
     
+    //Create multiplying vector
+    let mut x_vector: Vec<f64> = vec![0.0 ; sp_matrix.get_columns_number()];
+    
+    //Create resulting vector
+    let y_vector: Vec<f64> = vec![0.0 ; sp_matrix.get_rows_number()];
+    
+    fill_vector(&mut x_vector);
+
+    println!("x: {} y: {} ", x_vector[0], y_vector[0] );
+
     //create the spmv kernel
     
+
     //measure times 
 
 }
